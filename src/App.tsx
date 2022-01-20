2c9f81c95ca2a60d1f18ae0fd82dc183528b349d
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2022-01-20 15:50:51 +0000

    Add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { PodcastUrl } from "./Components/PodcastUrl";
+
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello from React
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+      <Route path='/podcast-url' element={<PodcastUrl/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
