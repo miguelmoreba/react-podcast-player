13c47cc15251c70a28769f965160e6ad29fbc586
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2022-01-21 15:22:33 +0000

    Pick up url and attempting to play it for now

--- src/Components/PodcastUrl.tsx
+++ src/Components/PodcastUrl.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export const PodcastUrl = () => {
+  const [sanitisedUrl, setSanitisedUrl] = useState("");
+
+  const renderPlayer = () => {
+    if (sanitisedUrl === "") {
+      return;
+    } else if (!sanitisedUrl) {
+      return <p>Please enter a valid url</p>;
+    } else {
+      return (
+        <div style={{ marginTop: "20px" }} className="flex-element">
+          <audio id="audio-player" controls src={sanitisedUrl}>
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        </div>
+      );
+    }
+  };
+
   return (
-      <div className='box'>
-          <input type="text" className='flex-element'/>
-      </div>
+    <div className="box">
+      <input
+        type="text"
+        className="flex-element"
+        value={sanitisedUrl}
+        onInput={(e: React.ChangeEvent<HTMLInputElement>) => setSanitisedUrl(e.target.value)}
+      />
+      {renderPlayer()}
+    </div>
   );
 };
